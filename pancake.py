8852ea36ce911e390412d78d14c6efa3098cd6e8
jmaloyjr <jmaloyjr@udel.edu>
2019-03-13 22:35:32 -0400

    Finished graph creation and DFS working on UCS

--- pancake.py
+++ pancake.py
@@ -18,11 +18,11 @@ def checkList(arr):
 
 ### Running desiered algorithm based off last char. ###
 def runAlgorithm(arr):
-    if(arr[4] == "d"):
+    if(arr.name[4] == "d"):
         DFS(arr)
-    elif(arr[4] == "g"):
+    elif(arr.name[4] == "g"):
         greedy(arr)
-    elif(arr[4] == "a"):
+    elif(arr.name[4] == "a"):
         aStar(arr)
     else:
         UCS(arr)
@@ -59,7 +59,12 @@ def flip(arr, i):
 
 ### Check which number is larger to determine next move. ###
 def tieBreaker(arr1, arr2):
-    print("tiebreaker function\n")
+    for i in range(4):
+        if(int(arr1.name[i]) > int(arr2.name[i])):
+            return arr1
+        elif(int(arr1.name[i]) < int(arr2.name[i])):
+            return arr2
+
 
 ### Function to check tree for already seen values. ###
 def checkTreeForDups(tree, stack):
@@ -67,9 +72,13 @@ def checkTreeForDups(tree, stack):
     ## If a success add to tree to become noticable, duplicate winners are fine and expected ## 
     if(checkSuccess(stack)):
         return False
-    
+
     ## Array of nodes will be a test array to see if it exists ## 
-    arrayOfNodes = anytree.find(tree, lambda nodes: nodes.name == stack)
+    try:
+        arrayOfNodes = anytree.find(tree, lambda nodes: nodes.name == stack)
+    except: 
+        return True
+    
     if(arrayOfNodes > 0):
         return True
     else:
@@ -85,41 +94,88 @@ def createTree(arr):
     for x in range(3):
         tmp = arr[:]
         flip(tmp, x + 2)
-        tmpNode = Node(tmp, parent=ROOT)
-        makeLeaves(tmp, tmpNode, ROOT)
+        tmpNode = Node(tmp, parent=ROOT, g=x+1)
+        makeLeaves(tmp, tmpNode, ROOT, 0)
         global recursionVAR
         recursionVAR = 0
 
     ## Print Tree for viewing purposes ##
-    print(RenderTree(ROOT))
+
+    for pre, _, node in RenderTree(ROOT):
+        print("%s%s" % (pre, node.name))
+
+    print("\n ######################## GRAPH #############################")
+
+    return ROOT
 
 ### Recursive function to make leaves ###
-def makeLeaves(arr, node, rootNode):
+def makeLeaves(arr, node, rootNode, iterator):
     ROOT = rootNode
     global recursionVAR
     recursionVAR = recursionVAR + 1
 
     ## If what we are looking at is a success dont continue ## 
     if(checkSuccess(arr) != True):
-        for x in range(3):
+        for x in range(4):
             tmp = arr[:]
             flip(tmp, x+1)
-
             ## Check for duplicates before continuing ##
-            if(checkTreeForDups(ROOT, tmp) == False):
-                tmpNode = Node(tmp, parent=node)
-
+            if(tmp != arr):
+                tmpNode = Node(tmp, parent=node, g=x+1)
                 ## Set recursion value, used for testing will change when bigger trees are required ## 
-                if(recursionVAR < 3):
-                    makeLeaves(tmp, tmpNode, rootNode)
-    else:
-        ## Testing Print ## 
-        print("WINNER WINNER")
+                if(iterator < 2):
+                    iterator = iterator + 1
+                    makeLeaves(tmp, tmpNode, rootNode, iterator)
+                    print(tmpNode.g)
     
+
+def printProcess(arr, i):
+    output = ""
+    for x in range(4):
+        if(x == i):
+            output+="|"
+            output+=str(arr[x])
+        else:
+            output+=str(arr[x])
+    print(output)
+
+def findDifference(parent, child):
+    for i in range(4):
+        if(parent[i] != child[i]):
+            return i
+
+##### Implementing Fringe #####
 
 ### DFS Algorithm. ###
 def DFS(arr):
-    print("dfs")
+    goalNode = arr
+
+    ## Run DFS until goal node is found. ##    
+    while(checkSuccess(goalNode.name) == False):
+        children = goalNode.children
+        totalChildren = len(children)
+
+        ## If only one child print and continue. ##
+        if(totalChildren == 1):
+            num = findDifference(goalNode.name, children.name)
+            printProcess(goalNode.name, num)
+            goalNode = children
+
+        ## For two or more children run a tie breaker and print result. ##
+        elif(totalChildren == 2):
+            originalNode = goalNode
+            goalNode = tieBreaker(children[0], children[1])
+            num = findDifference(originalNode.name, goalNode.name)
+            printProcess(originalNode.name, num)
+        elif(totalChildren == 3):
+            originalNode = goalNode
+            tmp = tieBreaker(children[0], children[1])
+            goalNode = tieBreaker(tmp, children[2])
+            num = findDifference(originalNode.name, goalNode.name)
+            printProcess(originalNode.name, num)
+
+    ## Printing final process node ##
+    printProcess(goalNode.name, 10)
     
 
 ### A* Algorithm. ###
@@ -132,7 +188,7 @@ def greedy(arr):
 
 ### UCS Algorithm. ###
 def UCS(arr):
-    print("UCS")
+    print("working on it")
 
 
 
@@ -145,9 +201,10 @@ def main():
     checkList(stack)
 
     ### Create the tree ###
-    createTree(stack)
-
-    runAlgorithm(stack)
+    ROOT = createTree(stack)
+    
+    ### Run algorithm as determined by user ###
+    runAlgorithm(ROOT)
     
 ### Python stuff ya know. ###
 if __name__ == "__main__":
